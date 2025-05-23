69c86f36415e1bda826f521f4df56e57d747bd3d
ArindamBiswas10 <arindombiswas29@gmail.com>
2025-05-23 23:54:06 +0530

    chore: add footer with contact info to homepage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 
-export default function Home(){
-  return(
-    <main style = {{ padding: '2rem'}}>
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
       <h1>PingPulse</h1>
       <p>Building industry-grade infra with AWS and Serverless</p>
-      <Link href='/about'>Go to About Page</Link>
+      <Link href="/about">Go to About Page</Link>
+
+      <footer style={{ marginTop: '2rem', fontSize: '0.8rem', color: '#666' }}>
+        © 2025 Arindam Biswas | Contact: arindombiswas29@gmail.com
+      </footer>
     </main>
   );
 }
